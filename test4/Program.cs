ca40d5e8f380a28018d13652f181e1e7c8a091fc
Tatyana Burdienko <Tatyana.mishkileeva@mail.ru>
2024-02-09 16:53:28 +0900

    add HW:test4

---
+++ test4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход
+// принимает натуральное число N, а на выходе
+// показывает его цифры через запятую.
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+string s = "num";
+for (int num = 0; num < s.Lenght; num++)
+{
+  Console.Write($"{s[num]}, ");  
+}
+Console.Write("\b\b ");
